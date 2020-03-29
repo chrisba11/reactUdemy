d625eaabbdfac0ac16647f49568f7f1eecd94c3e
Chris Ball <chrisball7@gmail.com>
2020-03-29 15:11:57 -0700

    replacing functional App component with class based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi There!</div>
+
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
+  return <div>Latitude: </div>
 };
+
+class App extends React.Component {
+  render() {
+    return <div>Latitude: </div>
+  }
+}
 
 ReactDOM.render(
   <App />,
