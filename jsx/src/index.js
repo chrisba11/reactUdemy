f341ef697e837c3081601299072a0f1ed4691158
Chris Ball <chrisball7@gmail.com>
2020-03-28 20:52:37 -0700

    added imports of react and reactDOM

---
+++ jsx/src/index.js
@@ -0,0 +1,8 @@
+// Import the React and ReactDOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React Component
+
+
+// Take the react component and show it on the screen
