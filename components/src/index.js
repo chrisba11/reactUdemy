b96d8783dd8a7beab289d4896ac1cfc78b484f4b
Chris Ball <chrisball7@gmail.com>
2020-03-29 00:03:11 -0700

    working with approval cards and wrapping them around comments

--- components/src/index.js
+++ components/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const user1 = {
   first: faker.name.firstName(),
@@ -40,36 +41,52 @@ const user4 = {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author={user1.fullName()}
-        avatar={user1.avatar} 
-        timeStamp="Today at 6pm" 
-        content={faker.lorem.sentences()}
-      />
-      <CommentDetail 
-        author={user2.fullName()} 
-        avatar={user2.avatar} 
-        timeStamp="Today at 6pm" 
-        content={faker.lorem.sentences()}
-      />
-      <CommentDetail 
-        author={user1.fullName()} 
-        avatar={user1.avatar} 
-        timeStamp="Today at 6pm" 
-        content={faker.lorem.sentence()}
-      />
-      <CommentDetail 
-        author={user3.fullName()} 
-        avatar={user3.avatar} 
-        timeStamp="Today at 6pm" 
-        content={faker.lorem.sentences()}
-      />
-      <CommentDetail 
-        author={user4.fullName()} 
-        avatar={user4.avatar} 
-        timeStamp="Today at 6pm" 
-        content={faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author={user1.fullName()}
+          avatar={user1.avatar} 
+          timeStamp="Today at 6pm" 
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author={user2.fullName()} 
+          avatar={user2.avatar} 
+          timeStamp="Today at 6pm" 
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author={user1.fullName()} 
+          avatar={user1.avatar} 
+          timeStamp="Today at 6pm" 
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author={user3.fullName()} 
+          avatar={user3.avatar} 
+          timeStamp="Today at 6pm" 
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author={user4.fullName()} 
+          avatar={user4.avatar} 
+          timeStamp="Today at 6pm" 
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
