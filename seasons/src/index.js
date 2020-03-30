c0bed3bada7992bb48b7e1c04b4e331e58c5e3cb
Chris Ball <chrisball7@gmail.com>
2020-03-29 17:30:05 -0700

    moved state initialization outside of constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor (props) {
-    super(props);
 
-    this.state = { lat: null, errorMessage: '' };
-
-  }
+  state = { lat: null, errorMessage: '' }
   
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -27,6 +23,7 @@ class App extends React.Component {
 
     return <div>Loading...</div>
   }
+
 }
 
 ReactDOM.render(
