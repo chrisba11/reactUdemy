a6e75843c21d0df886cef726059e256a257deb60
Chris Ball <chrisball7@gmail.com>
2020-03-29 20:17:42 -0700

    moved render function logic into helper function and called that function inside render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,8 +13,8 @@ class App extends React.Component {
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-  
-  render() {
+
+  renderContent () {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -24,6 +24,14 @@ class App extends React.Component {
     }
 
     return <LoadingSpinner message="Please accept location request..." />
+  }
+  
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 
 }
