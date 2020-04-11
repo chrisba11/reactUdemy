42a5e84a747903ab4488b96420c2ebc366f9e2de
Chris Ball <chrisball7@gmail.com>
2020-04-11 15:16:46 -0700

    working with async response

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-         Authorization: 'Client-ID q-STLrNeryp9wY4R3ivL7LskZXF8fM1Egvjp4kSYHPY'
-       }
-    }).then((response) => {
-      console.log(response.data.results);
-    });
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID q-STLrNeryp9wY4R3ivL7LskZXF8fM1Egvjp4kSYHPY'
+        }
+      });
+    console.log(response.data.results);
   }
 
   render() {
