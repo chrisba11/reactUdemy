e30f34567e04650a5278e5a7484b06979a0bc82b
Chris Ball <chrisball7@gmail.com>
2020-04-12 20:13:11 -0700

    added code to show the number of images found, but not handling 'this' correctly, so getting an error

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -3,21 +3,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] };
+
+
   async onSearchSubmit(term) {
-    const response = await axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization: 'Client-ID q-STLrNeryp9wY4R3ivL7LskZXF8fM1Egvjp4kSYHPY'
-        }
-      });
-    console.log(response.data.results);
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID q-STLrNeryp9wY4R3ivL7LskZXF8fM1Egvjp4kSYHPY'
+      }
+    });
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
        </div>
     )
   }
