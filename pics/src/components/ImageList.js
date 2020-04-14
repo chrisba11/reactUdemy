f0eda5aa84ebd54769971ec26a7a4aa64507abdd
Chris Ball <chrisball7@gmail.com>
2020-04-13 20:42:07 -0700

    rendering list of images with alt description and keys

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
+  console.log(props.images);
   const images = props.images.map((image) => {
-    return <img alt='car' src={image.urls.regular} />
+    return <img key={image.id} alt={image.alt_description} src={image.urls.regular} />
   });
 
   return <div>{images}</div>;
