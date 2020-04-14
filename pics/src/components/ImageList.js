3288b151a5ba1d7fd3a54f0cd972a02c7d573683
Chris Ball <chrisball7@gmail.com>
2020-04-13 21:02:50 -0700

    created ImageCard component to replace img elements and adjusted props accordingly

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,12 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
+
 
 const ImageList = (props) => {
   console.log(props.images);
   const images = props.images.map((image) => {
-    return <img key={image.id} alt={image.alt_description} src={image.urls.regular} />
+    return <ImageCard key={image.id} image={image} />
   });
 
   return <div className="image-list">{images}</div>;
